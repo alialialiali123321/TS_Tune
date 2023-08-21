c58d81f03eb9c1bd8d94b0a12594d95bbdb9941e
haytham-alattar <haytham.alattar7@gmail.com>
2023-08-21 11:24:27 +0400

    Tune App

---
+++ lib/widgets/tune_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../models/tune_model.dart';
+
+class TuneItem extends StatelessWidget {
+  final TuneModel tune;
+  const TuneItem({super.key, required this.tune});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          tune.playSound();
+        },
+        child: Container(
+          color: tune.color,
+        ),
+      ),
+    );
+  }
+}
